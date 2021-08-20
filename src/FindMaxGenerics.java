675161c3da9ff88c0221d3c4c225fff136f8e951
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-20 23:26:44 +0530

    [ruchi]: Added for more than 3 find max using generics class

--- src/FindMaxGenerics.java
+++ src/FindMaxGenerics.java
@@ -1,18 +1,19 @@
 public class FindMaxGenerics<T extends Comparable<T>>{
 
-    T x,y,z;
+    T x,y,z,a;
 
-    public FindMaxGenerics(T x, T y, T z) {
+    public FindMaxGenerics(T x, T y, T z,T a) {
         this.x = x;
         this.y = y;
         this.z = z;
+        this.a=  a;
     }
 
     public T maximum(){
-        return FindMaxGenerics.maximum(x,y,z);
+        return FindMaxGenerics.maximum(x,y,z,a);
     }
 
-    public static <T extends Comparable<T>> T maximum(T x,T y,T z)
+    public static <T extends Comparable<T>> T maximum(T x,T y,T z,T a)
     {
 
         T max = x;
@@ -22,6 +23,9 @@ public class FindMaxGenerics<T extends Comparable<T>>{
 
         if (z.compareTo(max) > 0)
             max = z;
+
+        if (a.compareTo(max)>0)
+            max=a;
 
         printMax(x,y,z,max);
 
@@ -36,13 +40,13 @@ public class FindMaxGenerics<T extends Comparable<T>>{
     }
 
     public static void main(String args[]){
-        Integer xInt=3,yInt=8,zInt=7;
-        Float xF1=4.4f,yF1=5.1F,zF1=2.1F;
-        String xStr="ruchi",yStr="ravindra",zStr="mandve";
+        Integer xInt=3,yInt=8,zInt=7,aInt=12;
+        Float xF1=4.4f,yF1=5.1F,zF1=2.1F,aF1=1.1f;
+        String xStr="ruchi",yStr="ravindra",zStr="mandve",aStr="pratibha";
 
-        new FindMaxGenerics(xInt,yInt,zInt).maximum();
-        new FindMaxGenerics(xF1,yF1,zF1).maximum();
-        new FindMaxGenerics(xStr,yStr,zStr).maximum();
+        new FindMaxGenerics(xInt,yInt,zInt,aInt).maximum();
+        new FindMaxGenerics(xF1,yF1,zF1,aF1).maximum();
+        new FindMaxGenerics(xStr,yStr,zStr,aStr).maximum();
 
     }
 
