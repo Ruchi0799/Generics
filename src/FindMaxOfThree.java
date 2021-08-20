921e5ec0d2e57d90fb02210975574de0e37c6cd5
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-20 19:25:08 +0530

    [ruchi]: Added find max of Float method

--- src/FindMaxOfThree.java
+++ src/FindMaxOfThree.java
@@ -13,10 +13,31 @@ public class FindMaxOfThree {
         return max;
 
     }
+
+    public static float Findmax1(Float[] floatArray){
+        float max=floatArray[0];
+        if(floatArray[1].compareTo(max)>0)
+        {
+            max=floatArray[1];
+        }
+        if (floatArray[2].compareTo(max)>0)
+        {
+            max=floatArray[2];
+        }
+        return max;
+
+    }
+
     public static void main(String[] args) {
         Integer[] intArray={4,7,1};
+        Float[] floatArray={4.5f,5.0f,6.2f};
+
+
         int maxnumint=Findmax(intArray);
         System.out.println("Max Integer is: "+maxnumint);
+
+        float maxnumfloat=Findmax1(floatArray);
+        System.out.println("Max Float is: "+maxnumfloat);
 
 
     }
