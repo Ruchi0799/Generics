d77c2639197430389ff7874ab25b1b72c0ba3482
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-20 19:18:13 +0530

    [ruchi]: Added find max of Integer method

---
+++ src/FindMaxOfThree.java
@@ -0,0 +1,23 @@
+public class FindMaxOfThree {
+
+    public static int Findmax(Integer[] intArray){
+        int max=intArray[0];
+        if(intArray[1].compareTo(max)>0)
+        {
+            max=intArray[1];
+        }
+        if (intArray[2].compareTo(max)>0)
+        {
+            max=intArray[2];
+        }
+        return max;
+
+    }
+    public static void main(String[] args) {
+        Integer[] intArray={4,7,1};
+        int maxnumint=Findmax(intArray);
+        System.out.println("Max Integer is: "+maxnumint);
+
+
+    }
+}
--- src/Generics.java
+++ src/Generics.java
@@ -1,5 +1,5 @@
 public class Generics {
     public static void main(String[] args) {
-        
+
     }
 }
